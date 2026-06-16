a7cb8ef6c3669605178471b113a17faefb94bed6
winikfi <winikfred@gmail.com>
2026-06-16 06:56:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  publicDir: "public",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "./index.html"
-    }
-  }
 });
